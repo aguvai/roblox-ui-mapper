22a9933d3e94b90f1957e1c6687423269f7e1d88
aguvai <145935152+aguvai@users.noreply.github.com>
2025-09-07 14:31:44 -0400

    Implement Notification module

---
+++ UIHandling/OverlayHandling/Modules/Notification.lua
@@ -0,0 +1,91 @@
+local Preferences = require(script.Parent.Parent.Preferences)
+
+local TweenService = game:GetService("TweenService")
+local template = script:WaitForChild("NotificationTemplate")
+
+-- [[ BUILD FUNCTIONS ]] -- 
+-- // left side
+local function buildLeftSide(leftside, options)
+	leftside.Icon.Image = tonumber(options.icon_id)
+	if options.icon_text ~= nil and options.icon_text ~= "" then
+		leftside.Icon.IconText.Text = options.icon_text
+	end
+end
+
+-- // right side
+local function buildRightSide(rightside, options)
+	local buttonFrame = rightside.ButtonFrame
+	local buttonText = buttonFrame.ButtonText
+	local hitbox = buttonFrame.Hitbox
+	buttonText.Text = options.button_text
+	hitbox.MouseButton1Click:Once(options.button_action)
+	
+	local primaryText = rightside.PrimaryTextFrame.PrimaryText
+	primaryText.Text = options.primary_text
+end
+
+
+-- [[ TWEEN FUNCTIONS ]] --
+-- // Tween out
+function tweenOut(gui)
+	local tweenOut = TweenService:Create(gui, Preferences.Notification.TweenOutInfo, {
+		Position = UDim2.new(1.1, 0, 0, 0)
+	})
+	tweenOut:Play()
+
+	tweenOut.Completed:Once(function()
+		gui:Destroy()
+	end)
+end
+
+-- // Tween in
+local function tweenIn(gui)
+	gui.Visible = true
+	local tweenIn = TweenService:Create(gui, Preferences.Notification.TweenInInfo, {
+		Position = UDim2.new(0, 0, 0, 0)
+	})
+	tweenIn:Play()
+
+	task.spawn(function()
+		task.wait(Preferences.Notification.secondsOnScreen)
+		tweenOut(gui)
+	end)
+end
+
+
+
+local Notification = {}
+Notification.__index = Notification
+
+function Notification.new(overlayGUI, options)
+	local self = setmetatable({}, Notification)
+	self.options = options
+	
+	-- clone template
+	self.gui = template:Clone()
+	self.gui.Parent = overlayGUI:WaitForChild("NotificationFrame", 5)
+	self.gui.Position = UDim2.new(1.1, 0, 0, 0)
+
+	-- build
+	self.gui.Title.Text = options.title
+	
+	if options.icon_id ~= nil and options.icon_id ~= "" then
+		buildLeftSide(self.gui.MainFrame.LeftSide, self.options)
+	else
+		self.gui.MainFrame.LeftSide.Visible = false
+		self.gui.MainFrame.RightSide.Size = UDim2.new(.9, 0, .8, 0)
+	end
+	
+	buildRightSide(self.gui.MainFrame.RightSide, self.options)
+
+	-- animate in
+	tweenIn(self.gui)
+
+	return self
+end
+
+function Notification:Dismiss()
+	tweenOut(self.gui)
+end
+
+return Notification

33c7e918ab7ddda2cf78059c2903fca7d6aa4b61
aguvai <145935152+aguvai@users.noreply.github.com>
2025-09-07 18:11:03 -0400

    Transition to TweenService

--- UIHandling/ScreenButtonHandling/AnimationFunctions.lua
+++ UIHandling/ScreenButtonHandling/AnimationFunctions.lua
@@ -6,7 +6,6 @@ local Preferences = require(modules.Preferences)
 -- [[ SERVICES ]] --
 local TweenService = game:GetService("TweenService")
 
-
 -- [[ * MAIN * ]]
 local AnimationFunctions = {}
 
@@ -14,42 +13,53 @@ local AnimationFunctions = {}
 AnimationFunctions.openButton_hoverEnter = function(button)
 	local innerCircle = button.Parent:FindFirstChild("InnerCircle")
 	if innerCircle then
-		innerCircle:TweenSize(
-			UDim2.new(0.95, 0, 0.95, 0),
-			Enum.EasingDirection.InOut,
+		local tweenInfo = TweenInfo.new(
+			0.2,
 			Enum.EasingStyle.Quart,
-			.2,
-			true
+			Enum.EasingDirection.InOut
 		)
+		local tween = TweenService:Create(innerCircle, tweenInfo, {
+			Size = UDim2.new(0.95, 0, 0.95, 0)
+		})
+		tween:Play()
 	end
 end
 
 AnimationFunctions.openButton_hoverLeave = function(button)
 	local innerCircle = button.Parent:FindFirstChild("InnerCircle")
 	if innerCircle then
-		innerCircle:TweenSize(
-			UDim2.new(0.8, 0, 0.8, 0),
-			Enum.EasingDirection.InOut,
+		local tweenInfo = TweenInfo.new(
+			0.2,
 			Enum.EasingStyle.Quart,
-			.2,
-			true
+			Enum.EasingDirection.InOut
 		)
+		local tween = TweenService:Create(innerCircle, tweenInfo, {
+			Size = UDim2.new(0.8, 0, 0.8, 0)
+		})
+		tween:Play()
 	end
 end
 
 -- [[ CLOSE BUTTON ]] --
 AnimationFunctions.closeButton_hoverEnter = function(button)
 	if button.Parent then
-		button.Parent.BackgroundTransparency = .25
+		local tweenInfo = TweenInfo.new(0.15, Enum.EasingStyle.Linear, Enum.EasingDirection.Out)
+		local tween = TweenService:Create(button.Parent, tweenInfo, {
+			BackgroundTransparency = 0.25
+		})
+		tween:Play()
 	end
 end
 
 AnimationFunctions.closeButton_hoverLeave = function(button)
 	if button.Parent then
-		button.Parent.BackgroundTransparency = 0
+		local tweenInfo = TweenInfo.new(0.15, Enum.EasingStyle.Linear, Enum.EasingDirection.Out)
+		local tween = TweenService:Create(button.Parent, tweenInfo, {
+			BackgroundTransparency = 0
+		})
+		tween:Play()
 	end
 end
-
 
 -- [[ OPEN / CLOSE FRAMES ]]
 -- pull tween/position preferences from Preferences
@@ -73,11 +83,19 @@ AnimationFunctions.closeGUI = function(mainFrame)
 	if closingDebounce then return end
 	closingDebounce = true
 
-	local tween = mainFrame:TweenPosition(closedPosition, closeTween.easingDirection, closeTween.easingStyle, closeTween.time, true)
-	tween.Completed:Wait()
-	mainFrame.Visible = false
+	local tween = TweenService:Create(mainFrame, TweenInfo.new(
+		closeTween.time,
+		closeTween.easingStyle,
+		closeTween.easingDirection
+		), {
+			Position = closedPosition	
+		})
 
-	closingDebounce = false
+	tween:Play()
+	tween.Completed:Connect(function()
+		mainFrame.Visible = false
+		closingDebounce = false
+	end)
 end
 
 -- // Open function
@@ -89,7 +107,15 @@ AnimationFunctions.openGUI = function(mainFrame)
 
 	AnimationFunctions.clearGUIs()
 	mainFrame.Visible = true
-	mainFrame:TweenPosition(openedPosition, openTween.easingDirection, openTween.easingStyle, openTween.time, true)
+
+	local tween = TweenService:Create(mainFrame, TweenInfo.new(
+		openTween.time,
+		openTween.easingStyle,
+		openTween.easingDirection
+		), {
+			Position = openedPosition
+		})
+	tween:Play()
 end
 
 return AnimationFunctions

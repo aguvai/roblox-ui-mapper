9b7dbfd989af7a80cf9fb289df868a70041ba7a0
aguvai <145935152+aguvai@users.noreply.github.com>
2025-09-09 22:21:21 -0400

    Update Toast to have title and message components

--- UIHandling/OverlayHandling/Modules/Toast.lua
+++ UIHandling/OverlayHandling/Modules/Toast.lua
@@ -11,9 +11,9 @@ local TweenService = game:GetService("TweenService")
 local template = script:WaitForChild("ToastTemplate")
 
 local typeColors = {
-	["success"] = Color3.fromRGB(66, 226, 63),
-	["error"] = Color3.fromRGB(186, 15, 15),
-	["info"] = Color3.fromRGB(227, 227, 227),
+	["success"] = Color3.fromRGB(165, 217, 175),
+	["error"] = Color3.fromRGB(186, 80, 80),
+	["info"] = Color3.fromRGB(220, 220, 220),
 	["warning"] = Color3.fromRGB(255, 185, 44)
 }
 
@@ -24,11 +24,7 @@ local function normalizeText(options)
 		textColor = typeColors.info
 		options.type = "info" 
 	end
-	
-	if options.type ~= "info" then
-		options.primary_text = string.format("%s: %s", string.upper(options.type), options.primary_text)
-	end
-	
+
 	return textColor
 end
 
@@ -38,24 +34,34 @@ Toast.__index = Toast
 function Toast.new(overlayGUI, options)
 	local self = setmetatable({}, Toast)
 
-    local textColor = normalizeText(options)
+	local textColor = normalizeText(options)
 
 	-- clone template
 	self.gui = template:Clone()
 	self.gui.Parent = overlayGUI:WaitForChild("ToastFrame", 5)
 	
+	if options.play_notification_sound ~= nil and options.play_notification_sound == true then
+		self.gui.Parent.Notification:Play()
+	end
+	
+	if options.type == "success" then
+		self.gui["2_Message"].Visible = false
+	end
+	
 	-- initialize
-	self.gui.Message.TextColor3 = textColor
+	self.gui["1_Title"].TextColor3 = textColor
 	self.gui.BackgroundTransparency = 1
-	self.gui.Message.TextTransparency = 1
-	
+	self.gui["2_Message"].TextTransparency = 1
+	self.gui["1_Title"].TextTransparency = 1
+
 	self.gui.Visible = true
-	
+
 	-- instance state
 	self.options = options
 
 	-- set primary text
-	self.gui.Message.Text = options.primary_text
+	self.gui["2_Message"].Text = options.primary_text
+	self.gui["1_Title"].Text = options.title or ""
 
 	-- animate in
 	self:tweenIn()
@@ -66,18 +72,20 @@ end
 -- [[ TWEEN FUNCTIONS ]] --
 -- // Tween in
 function Toast:tweenIn()
-	-- tween background transparency in to .92
 	local tweenBackground = TweenService:Create(self.gui, Preferences.Toast.TweenInInfo, {
-		BackgroundTransparency = .92
+		BackgroundTransparency = .825
 	})
 	tweenBackground:Play()
-	
+
 	-- tween message transparency in to 0
-	local tweenText = TweenService:Create(self.gui.Message, Preferences.Toast.TweenInInfo, {
+	local tweenText = TweenService:Create(self.gui["2_Message"], Preferences.Toast.TweenInInfo, {
 		TextTransparency = 0
 	})
 	tweenText:Play()
-	
+	TweenService:Create(self.gui["1_Title"], Preferences.Toast.TweenInInfo, {
+		TextTransparency = 0
+	}):Play()
+
 	task.spawn(function()
 		task.wait(Preferences.Toast.secondsOnScreen)
 		self:tweenOut()
@@ -85,16 +93,21 @@ function Toast:tweenIn()
 end
 
 function Toast:tweenOut()
+	if not self.gui then return end
+	
 	local tweenBackground = TweenService:Create(self.gui, Preferences.Toast.TweenOutInfo, {
 		BackgroundTransparency = 1
 	})
 	tweenBackground:Play()
-	
-	local tweenText = TweenService:Create(self.gui.Message, Preferences.Toast.TweenOutInfo, {
+
+	local tweenText = TweenService:Create(self.gui["2_Message"], Preferences.Toast.TweenOutInfo, {
 		TextTransparency = 1
 	})
 	tweenText:Play()
-	
+	TweenService:Create(self.gui["1_Title"], Preferences.Toast.TweenOutInfo, {
+		TextTransparency = 1
+	}):Play()
+
 	tweenBackground.Completed:Once(function()
 		self.gui:Destroy()
 	end)
